7b4f86f8b7dc4da79e4b9d58fc31aeca16bef477
MaxFeskov <maxfeskov@yandex.ru>
2018-05-11 02:56:41 +0300

    Удаление eslint

--- gulpfile.js
+++ gulpfile.js
@@ -22,11 +22,22 @@ gulp.task('clean', gulp.parallel('clean:image', 'clean:html', 'clean:inline', 'c
 
 gulp.task(
   'build',
-  gulp.series('clean', 'build:image', 'build:style', 'build:html', 'build:inline'),
+  gulp.series(
+    'clean',
+    'build:image',
+    'build:style',
+    'build:html',
+    'build:inline'
+  ),
 );
 
 gulp.task('watch', gulp.parallel('watch:image', 'watch:style', 'watch:html', 'watch:inline'));
 
-gulp.task('dev', gulp.series('dev:image', 'dev:style', 'dev:html', 'dev:inline'));
+gulp.task('dev', gulp.series(
+  'dev:image',
+  'dev:style',
+  'dev:html',
+  'dev:inline'
+));
 
 gulp.task('default', gulp.series('dev', gulp.parallel('server:init', 'watch')));
